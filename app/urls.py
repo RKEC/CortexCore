8683d8278d29ccc13a9cd350e7fefc80a29638fe
RKEC <r.kieran.collins@gmail.com>
2023-10-29 21:55:42 +0000

    Added ability to update idea

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('idea/', views.IdeaListView.as_view(), name="idea"),
     path('search/', views.IdeaSearchView.as_view(), name='idea_search'),
     path('add/', views.IdeaCreateView.as_view(), name='add_idea'),
+    path('idea/update/<int:pk>/', views.IdeaUpdateView.as_view(), name='update_idea'),
     path('delete/<int:pk>/', views.IdeaDeleteView.as_view(), name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, redirect
-from django.views.generic import DetailView, ListView, DeleteView, CreateView
+from django.views.generic import DetailView, ListView, DeleteView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import Idea
 from .forms import AddIdea
 from django.db.models import Q
 
-
 def index(request):
     return render(request, "index.html")
 
 
 def delete(request, pk):
-    return render(request, "idea_confirm_delete.html", {"id":pk})
+    return render(request, "idea_confirm_delete.html", {"id": pk})
 
 
 class IdeaCreateView(CreateView):
@@ -55,6 +54,22 @@ class IdeaSearchView(ListView):
         if search_idea:
             return Idea.objects.filter(Q(title__icontains=search_idea))
         return Idea.objects.all().order_by("-dateAdded")
+
+
+class IdeaUpdateView(UpdateView):
+    model = Idea
+    form_class = AddIdea  # Using the form class you've defined
+    template_name = 'update-idea.html'  # Specify your update template
+    success_url = reverse_lazy('idea')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The idea was updated successfully.")
+        return super(IdeaUpdateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(IdeaUpdateView, self).get_context_data(**kwargs)
+        # You can add additional context here if needed
+        return context
 
 
 class IdeaDeleteView(DeleteView):
